bee27534a92afd124ca916bd61a268ef1c612f74
SHAHRULLOHON KHURSHEDKHO LUTFILLOHONOV <shahrullo777@gmail.com>
2024-07-05 16:46:17 +0900

    basic functionalities are included for orientation and  operation

--- render-hex-threadpool/src/main.rs
+++ render-hex-threadpool/src/main.rs
@@ -1,3 +1,117 @@
+use core::str;
+use std::env;
+
+use svg::node::element::path::{Command, Data, Position};
+use svg::node::element::{Path, Rectangle};
+use svg::Document;
+
+use crate::Operation::{
+    Forward,
+    Home,
+    Noop,
+    TurnLeft,
+    TurnRight,
+};
+
+use crate::Orientation::{
+    East,
+    North,
+    South,
+    West
+}
+
+const WIDTH: isize = 400;
+const HEIGHT: isize = WIDTH;
+
+const HOME_Y: isize = HEIGHT / 2;
+const HOME_X: isize = WIDTH / 2;
+
+const STROKE_WIDTH: usize = 5;
+
+#[derive(Debug, Clone, Copy)]
+enum Orientation {
+    North,
+    East,
+    West,
+    South
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Operation {
+    Forward(isize),
+    TurnLeft,
+    TurnRight,
+    Home,
+    Noop(u8)
+}
+
+#[derive(Debug)]
+struct Artist {
+    x: isize,
+    y: isize,
+    heading: Orientation
+}
+
+impl Artist {
+    fn new() -> Artist {
+        Artist {
+            heading: North,
+            x: HOME_X,
+            y: HOME_Y,
+        }
+    }
+
+    fn home(&mut self) {
+        self.x = HOME_X;
+        self.y = HOME_Y;
+    }
+
+    fn forward(&mut self, distance: isize) {
+        match self.heading {
+            North => self.y += distance,
+            South => self.y -= distance,
+            West => self.x += distance,
+            East => self.x -= distance,
+        }
+    }
+
+    fn turn_right(&mut self) {
+        self.heading = match self.heading {
+            North => East,
+            South => West,
+            West => North,
+            East => South,
+        }
+    }
+
+    fn turn_left(&mut self) {
+        self.heading = match self.heading {
+            North => West,
+            South => East,
+            West => South,
+            East => North,
+        }
+    }
+
+    fn wrap(&mut self) {
+        if self.x < 0 {
+            self.x = HOME_X;
+            self.heading = West;
+        } else if self.x > WIDTH {
+            self.x = HOME_X;
+            self.heading = East;
+        }
+
+        if self.y < 0 {
+            self.y = HOME_Y;
+            self.heading = North;
+        } else if self.y > HEIGHT {
+            self.y = HOME_Y;
+            self.heading = South;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    todo!()
 }
